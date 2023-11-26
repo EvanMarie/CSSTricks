468c9cdcaeaf901057479463b053d525027bcb9c
Evan Marie Carr <evanmariecarr@gmail.com>
2023-11-26 15:26:25 -0500

    switching

--- index.js
+++ index.js
@@ -29,6 +29,8 @@ window.addEventListener("resize", () => {
   displayContainerWidth(".responsive-container");
 });
 
+/* -------------------------------------------------------------------- */
+
 document.addEventListener("DOMContentLoaded", () => {
   const scrollContainer = document.querySelector(".scroll-container");
   const items = document.querySelectorAll(".scroll-snap-item");
@@ -56,3 +58,45 @@ document.addEventListener("DOMContentLoaded", () => {
     scrollToItem(currentIndex + 1); // Scroll to the next item
   });
 });
+
+/* -------------------------------------------------------------------- */
+
+function displayViewportWidth(containerSelector) {
+  const containers = document.querySelectorAll(containerSelector);
+
+  containers.forEach((container) => {
+    // Create a div to display the viewport width or use an existing one
+    let widthDisplay = container.querySelector(".viewport-width-display");
+    if (!widthDisplay) {
+      widthDisplay = document.createElement("div");
+      widthDisplay.className = "viewport-width-display"; // Add a class for styling
+      container.appendChild(widthDisplay);
+    }
+
+    // Update the inner text of the div to the current viewport width
+    widthDisplay.textContent = `Viewport: ${window.innerWidth}px`;
+
+    // Style the div
+    widthDisplay.style.position = "absolute";
+    widthDisplay.style.top = "0";
+    widthDisplay.style.right = "0";
+    widthDisplay.style.backgroundColor = "var(--dark-pink)";
+    widthDisplay.style.color = "white";
+    widthDisplay.style.padding = "5px 10px";
+    widthDisplay.style.boxShadow = "var(--shadow)";
+    widthDisplay.style.textShadow = "var(--shadow)";
+    widthDisplay.style.fontSize = "12px";
+  });
+}
+
+// Run the function when the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", () => {
+  displayViewportWidth(".screen-width-container");
+});
+
+// Update width display on window resize
+window.addEventListener("resize", () => {
+  displayViewportWidth(".screen-width-container");
+});
+
+/* -------------------------------------------------------------------- */
